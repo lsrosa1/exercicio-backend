b99387e9f745bd2f81c4632e178f7de8726f525e
Igor Jose <igor.cordeiro@philips.com>
2022-11-03 13:43:47 -0300

    feat: adicionando tratamento de error

---
+++ src/main/java/com/example/desafio/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.desafio.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ClienteNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ApiError handlerNoClienteFoundException(ClienteNotFoundException ex) {
+        return ApiError.builder().menssage("Cliente Não foi encontrado").status(String.valueOf(HttpStatus.NOT_FOUND.value())).build();
+    }
+}
--- src/main/java/com/example/desafio/service/ClienteServiceImpl.java
+++ src/main/java/com/example/desafio/service/ClienteServiceImpl.java
@@ -1,31 +1,27 @@
 package com.example.desafio.service;
 
+import com.example.desafio.exception.ClienteNotFoundException;
 import com.example.desafio.model.Cliente;
 import com.example.desafio.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ClienteServiceImpl implements ClienteService {
 
     private final ClienteRepository clienteRepository;
-    private final EnderecoRepository enderecoRepository;
 
     @Override
     public List<Cliente> listar(){
-        List<Cliente> clientes = new ArrayList<>();
-        clienteRepository.findAll().forEach(clientes::add);
-        return clientes;
+        return clienteRepository.findAll();
     }
 
     @Override
     public Cliente obterPorId(Long id) {
-        return clienteRepository.findById(id).orElseThrow(() -> new RuntimeException("Cliente não foi encontrado"));
+        return clienteRepository.findById(id).orElseThrow(ClienteNotFoundException::new);
     }
 
     @Override
@@ -33,38 +29,27 @@ public class ClienteServiceImpl implements ClienteService {
         try {
             clienteRepository.save(cliente);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
             throw new RuntimeException("Erro ao tentar criar Cliente");
         }
     }
 
     @Override
     public void remover(Long id) {
-        Optional<Cliente> cliente = clienteRepository.findById(id);
-
-        if(!cliente.isPresent()) {
-            throw new RuntimeException("Cliente não foi encontrado");
-        }
-
+        clienteRepository.findById(id).orElseThrow(ClienteNotFoundException::new);
         clienteRepository.deleteById(id);
     }
 
     @Override
     public Cliente atualizar(Long id, Cliente novoCliente) {
-
-        try {
-            return clienteRepository.findById(id).map(c -> {
+        return clienteRepository.findById(id).map(c -> {
                 c.setCpf(novoCliente.getCpf());
                 c.setNome(novoCliente.getNome());
                 c.setSobrenome(novoCliente.getSobrenome());
                 c.setDataNascimento(novoCliente.getDataNascimento());
-
+                c.setEnderecos(novoCliente.getEnderecos());
                 return clienteRepository.save(c);
-            }).orElseThrow(() -> new RuntimeException("Cliente não foi encontrado"));
+            }).orElseThrow(ClienteNotFoundException::new);
 
-        } catch (Exception e) {
-            throw new RuntimeException("Erro ao tentar atualizar Cliente");
-        }
 
     }
 }
